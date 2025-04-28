ed4c918d3e8a1e9d9fe504ce574d35181ffb9275
tomas <tomasdesindes1@gmail.com>
2025-04-28 20:19:45 -0300

    -m "final commit"

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 document.getElementById("color").addEventListener("click", changeBackground);
 
-function changeBackground(){
-    
-    let symbols=["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+function changeBackground() {
 
-    let color="#";
+    let symbols = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
 
-    for (let i=0;i<6;i++){  
-        color=color+symbols[Math.floor(Math.random() * 16)];
+    let color = "#";
+
+    for (let i = 0; i < 6; i++) {
+        color = color + symbols[Math.floor(Math.random() * 16)];
     }
 
     console.log(color);
+
+    document.body.style.background = color;
+    document.querySelector("span").innerHTML=color;
+
 }   
